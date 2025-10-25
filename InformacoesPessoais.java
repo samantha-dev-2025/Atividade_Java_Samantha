81fa938b54c9c42c17d55fd4535ed1110d53642f
Samantha <samanthamellyssam@gmail.com>
2025-10-24 23:33:30 -0300

    informacoesPessoais

---
+++ InformacoesPessoais.java
@@ -0,0 +1,11 @@
+public class InformacoesPessoais {
+    public static void main(String arg[]){
+
+        String nome = "Ana";
+        Integer idade = 17;
+        Double altura = 1.65;
+
+        System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos e minha altura é " + altura + "m.");
+
+    }
+}
